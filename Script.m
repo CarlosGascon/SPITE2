a12d96bcb36bb006a8f11739b673a7b6d7677955
CarlosGascon <carlosgascon1996@gmail.com>
2019-02-19 12:55:26 -0500

    TargetList changes

--- Script.m
+++ Script.m
@@ -8,23 +8,16 @@ TargetList1 = {'24 Sex', ['b', 'c'];
               '55 Cnc', ['b', 'c', 'f']
               'HD 10180', ['c', 'e', 'f', 'h']};
           
-TargetList2 = {'11 Com', ['b'] 
-               '14 And', ['b']
-               '18 Del', ['b']
-               '24 Boo', ['b']
-               '24 Sex', ['b']
-               '47 UMa', ['b']
-               '55 Cnc', ['b']
-               '6 Lyn',  ['b']
-               '61 Vir', ['b']
-               '7 CMa',  ['b']
-               '70 Vir', ['b']
-               'GJ 163', ['b']
-               'GJ 667 C',['b']
-               'GJ 674', ['b']
-               'GJ 876', ['b']
-               'HATS-4', ['b']
-               'HATS-5', ['b']};  
+TargetList2 = {'75 Cet',  ['b']
+               '81 Cet',  ['b']
+               '91 Aqr',  ['b']
+               'GJ 179',  ['b']
+               'HD 45364',['b', 'c']
+               'HD 9174', ['b']
+               'HD 96167',['b']
+               'HIP 74890', ['b']
+               'HIP 67537', ['b']};
+
 TargetList3 = {'47 UMa', ['b', 'c', 'd']};
 
 TargetList4 = {'47 UMa', ['b', 'd']
@@ -32,7 +25,7 @@ TargetList4 = {'47 UMa', ['b', 'd']
                '55 Cnc', ['b', 'c', 'f']
                'HD 27894',['b', 'c']};
 
-Norb = 50;
+Norb = 80;
 
 YearsSim = 1e7;
 
@@ -42,4 +35,4 @@ Nexo = 1;
 
 %% Run Simulation and Output
 
-Stability = Run_Sims(TargetList3, Nexo, Norb, YearsSim, Ncores);
+Stability = Run_Sims(TargetList2, Nexo, Norb, YearsSim, Ncores);
